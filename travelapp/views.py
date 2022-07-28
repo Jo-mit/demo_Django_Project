2b7093263c9f0b29522cbfb0b69f80f030356920
Jo-mit <george.jr1509@gmail.com>
2022-07-29 01:24:17 +0530

    Initial commit

---
+++ travelapp/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Place
+from .models import Person
+
+# Create your views here.
+def demo(request):
+    obj=Place.objects.all()
+    objt=Person.objects.all()
+    return render(request,"index.html",{'result':obj,'results':objt})
+
